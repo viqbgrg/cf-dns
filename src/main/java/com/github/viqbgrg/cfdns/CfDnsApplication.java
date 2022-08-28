a5e4778d26e392fa0cd830b060dc9992a9b5b824
viqbgrg <viqbgrg@gmail.com>
2022-08-28 14:58:29 +0800

    first commit

---
+++ src/main/java/com/github/viqbgrg/cfdns/CfDnsApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.cfdns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDnsApplication.class, args);
+	}
+
+}
